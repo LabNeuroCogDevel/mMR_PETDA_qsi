1d2158906d67f39b8cbc343872f1195bc72c635d
WillForan <willforan@gmail.com>
2020-04-14 14:21:55 -0400

    🐛 txt is actually csv

--- recon_track/extract.R
+++ recon_track/extract.R
@@ -4,7 +4,7 @@ allsubjs <- Sys.glob('1*2*_*.nii.stat.txt')
 readsubjs <- function(f) read.table(f,sep="\t") %>% mutate(visit=str_extract(f,'\\d+_\\d+'))
 d <- lapply(allsubjs, readsubjs) %>% bind_rows()
 names(d) <- c('var','val','visit')# then to get just one variable
-write.csv(d,'all.stat.txt',row.names=F, quote=F)
+write.csv(d,'all.stat.csv',row.names=F, quote=F)
 
-d <- read.csv('all.stat.txt')
+d <- read.csv('all.stat.csv')
 d %>% filter(grepl('nrdi12L sd',var))
